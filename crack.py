b7a96056ecfd26de1a5584794c17b51745da7759
miikkalaitinen <38249083+miikkalaitinen@users.noreply.github.com>
2022-09-20 15:29:26 +0300

    added charset selection

--- crack.py
+++ crack.py
@@ -8,7 +8,21 @@ start = int(input("Starting row (0 for first):"))
 lowerlimit = int(input("Starting lenght:"))
 upperlimit = int(input("Upper limit:"))
 df = df.iloc[start: , :]
+set = (input("Select set: \nlowerase=1\nuppercase=2\nnumbers=3\nspecial=4\n\nAnswer as cobination as string e.g. 123: "))
+res = [int(x) for x in str(set)]
+#chars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"," ","!","?",".",",","*","_","-"]
 
+chars = []
+if 1 in res:
+    chars += ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+if 2 in res:
+    chars += ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+if 3 in res:
+    chars += ["1","2","3","4","5","6","7","8","9","0"]
+if 4 in res: 
+    chars += [" ","!","?",".",",","*","_","-"]
+
+print(chars)
 
 def hash(password, salt):
     i = f"potPlantSalt{password}{salt}"
@@ -16,18 +30,16 @@ def hash(password, salt):
     return h[0:32]
 
 def crack(salt, real, user):
-    attempts = 0
-    chars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9"," ","!","?",".",",","*","_","-"]
     for password_length in range(lowerlimit, upperlimit):
         print(password_length)
         for guess in product(chars, repeat=password_length):
-            attempts += 1
             guess = ''.join(guess)
             if hash(guess, salt) == real:
                 f = open("found.txt", "a")
-                f.write('password is {} for user {}. found in {} guesses. \n'.format(guess, user, attempts))
+                f.write('password is {} for user {}. \n'.format(guess, user))
                 f.close()
-                return 'password is {} for user {}. found in {} guesses.'.format(guess, user, attempts)
+                print('password is {} for user {}.'.format(guess, user))
+                return 0
 
 for index, row in df.iterrows():
     user = row["User"]
